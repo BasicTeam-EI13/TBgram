42c3039cc79c05fb5ab52af48389fd123140daa7
ParkKyoungMin <happypom88@gmail.com>
2025-02-18 20:45:13 +0900

    fix : 디렉토리명 수정

--- src/main/java/com/tbgram/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/tbgram/domain/comment/repository/CommentRepository.java
@@ -2,6 +2,12 @@ package com.tbgram.domain.comment.repository;
 
 import com.tbgram.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByNewsFeedIdOrderByCreatedAtDesc(Long newsFeedId);
 }
